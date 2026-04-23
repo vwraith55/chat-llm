818f04fc898ed0ab1d096a4782e30e087a3da5c8
Valerie Wraith <vwraith55@students.claremontmckenna.edu>
2026-04-23 15:41:35 -0700

    [docchat] Initial commit of test_wiggum.py

--- test_wiggum.py
+++ test_wiggum.py
@@ -1,7 +1,2 @@
-def add(a, b):
-    """This function adds two numbers, but not really."
-    return a + b
-
-# doctests
-print(add(2, 2))  # Should print 4
-print(add(2, 2))  # Should print 5
+def test_wiggum():
+    pass
